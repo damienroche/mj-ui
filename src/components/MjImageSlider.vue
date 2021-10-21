1c2a0eaafde75c0945bb7dfc444929dffd0ce08c
eliott pincemaile <eliott.pincemaile@gmail.com>
2021-10-21 09:50:39 +0200

    update component : mj-image-slider
    added no image and single image cases

--- src/components/MjImageSlider.vue
+++ src/components/MjImageSlider.vue
@@ -1,52 +1,62 @@
 <template>
   <div class="mj-image-slider relative">
     <div class="w-full relative overflow-hidden rounded">
-      <mj-aspect-ratio
-        v-for="(image, index) in images"
-        :key="index"
-        :image="image"
-        :aspect-ratio="aspectRatio"
-        :cover="cover"
-        class="w-full top-0 left-0 absolute transition-transform duration-300"
-        :class="{
-          'absolute': value !== index,
-          'translate-x-0 relative': value === index,
-          'translate-x-full': value < index,
-          '-translate-x-full': value > index,
-        }"
-      />
+      <template v-if="images.length > 0">
+        <mj-aspect-ratio
+          v-for="(image, index) in images"
+          :key="index"
+          :image="image"
+          :aspect-ratio="aspectRatio"
+          :cover="cover"
+          class="w-full top-0 left-0 absolute transition-transform duration-300"
+          :class="{
+            'absolute': value !== index,
+            'translate-x-0 relative': value === index,
+            'translate-x-full': value < index,
+            '-translate-x-full': value > index,
+          }"
+        />
+      </template>
+      <mj-flex-centered
+        v-else
+        class="bg-secondary"
+      >
+        <mj-text class="text-sm">Aucune image dans ce slider</mj-text>
+      </mj-flex-centered>
     </div>
-    <div
-      class="left-0"
-      :class="[
-        arrowClasses,
-        value > 0 ? 'cursor-pointer hover_text-primary' : 'opacity-50'
-      ]"
-      @click="$emit('input', value - 1 >= 0 ? value - 1 : 0)"
-    >
-      <mj-chevron-left />
-    </div>
-    <div
-      class="right-0"
-      :class="[
-        arrowClasses,
-        value < images.length - 1 ? 'cursor-pointer hover_text-primary' : 'opacity-50'
-      ]"
-      @click="$emit('input', value + 1 < images.length ? value + 1 : images.length - 1)"
-    >
-      <mj-chevron-right />
-    </div>
-    <div class="flex justify-center items-center absolute w-full bottom-4 drop-shadow">
+    <template v-if="images.length > 1">
       <div
-        v-for="index in images.length"
-        :key="index"
-        class="dot p-1 transition"
-        :class="{
-          'cursor-pointer opacity-50 hover_opacity-100 inactive': value !== index - 1
-        }"
-        @click="$emit('input', index - 1)"
-      ></div>
-    </div>
+        class="left-0"
+        :class="[
+          arrowClasses,
+          value > 0 ? 'cursor-pointer hover_text-primary' : 'opacity-50'
+        ]"
+        @click="$emit('input', value - 1 >= 0 ? value - 1 : 0)"
+      >
+        <mj-chevron-left />
+      </div>
+      <div
+        class="right-0"
+        :class="[
+          arrowClasses,
+          value < images.length - 1 ? 'cursor-pointer hover_text-primary' : 'opacity-50'
+        ]"
+        @click="$emit('input', value + 1 < images.length ? value + 1 : images.length - 1)"
+      >
+        <mj-chevron-right />
+      </div>
+      <div class="flex justify-center items-center absolute w-full bottom-4 drop-shadow">
+        <div
+          v-for="index in images.length"
+          :key="index"
+          class="dot p-1 transition"
+          :class="{
+            'cursor-pointer opacity-50 hover_opacity-100 inactive': value !== index - 1
+          }"
+          @click="$emit('input', index - 1)"
+        ></div>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -54,10 +64,12 @@
 import MjAspectRatio from './MjAspectRatio.vue';
 import MjChevronLeft from './icons/MjChevronLeft.vue';
 import MjChevronRight from './icons/MjChevronRight.vue';
+import MjText from '@/components/MjText.vue';
+import MjFlexCentered from '@/components/MjFlexCentered.vue';
 
 export default {
   name: 'MjImageSlider',
-  components: { MjChevronRight, MjChevronLeft, MjAspectRatio },
+  components: { MjFlexCentered, MjText, MjChevronRight, MjChevronLeft, MjAspectRatio },
   props: {
     /**
      * current slide index
